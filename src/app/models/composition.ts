849645780be7defe629e62f142e46f37846e68ae
ROMANO Cyril <romano.cyril@gmail.com>
2021-03-25 14:17:09 +0100

    Dev home page

--- src/app/models/composition.ts
+++ src/app/models/composition.ts
@@ -1,2 +1,17 @@
+import { Composer } from './composer'
+import { Collaboration } from './collaboration'
+import { Metronome } from './metronome'
+import { Track } from './track'
+
 export interface Composition {
+        id: number,
+        title: String,
+        createdDate: Date,
+        lastUpdate: Date,
+        playsNumber: number,
+        owner: Composer,
+        collaborations?: Collaboration[],
+        metronome?: Metronome,
+        tracks?: Track[],
+        blobPath: Blob
 }
